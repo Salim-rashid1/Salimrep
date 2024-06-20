45e97530b530ebe9461b9bb4f8bea817e6166093
Salim-rashid1 <salim.muchemi@strathmore.edu>
2024-06-20 15:19:39 +0300

    navigation php

---
+++ templates/nav.php
@@ -0,0 +1,10 @@
+<div class="toplink">
+            <a href="./">Home</a> | <a href="Form.html">About us</a> | <a href="box.html">boxtype</a> | 
+            <a href="project3.html">details</a> | <a href="./">Directions</a> | <a href="./">Foods</a> | 
+            
+            
+            <div class="toplink-right">
+                <a href="signup.html">signup</a>
+                <a href="signin.html">signin</a>
+            </div>
+        </div>
