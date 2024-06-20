ddcfd13893fe0bd515d9c1370e2f60ffec2ca09a
Salim-rashid1 <salim.muchemi@strathmore.edu>
2024-06-20 15:19:01 +0300

    changing html to php

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+        <title>Salim's Website 1</title>
+
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    
+    <body class="body_index_bg">
+
+    
+        <?php include_once ("templates/nav.php"); ?>
+
+        <div class="banner">
+            <h1>Heading 1</h1>
+        </div>
+
+        <div class="row">
+
+            <div class="content">
+                <ul>
+                    <li>black</li>
+                    <li>red</li>
+                    <li>pink</li>
+                    <li>purple</li>
+                </ul>
+        
+                <ol type="a">
+                    <li>salim</li>
+                    <li>rashid</li>
+                    <li>ali</li>
+                    <li>uzi</li>
+                </ol>
+        
+                <ol type="I" start="4">
+                    <li>black</li>
+                    <li>red</li>
+                    <li>pink</li>
+                    <li>purple</li>
+                </ol>
+        
+                <table border="1px" style="border: 1px solid
+                #373737; width: 50%; border-collapse: collapse ;">
+                    <tr>
+                        <td>Item quantity</td>
+                        <td>Quantity</td>
+                        <td>Unit price</td>
+                        <td>Total price</td>
+                    </tr>
+                    <tr>
+                        <td>Book</td>
+                        <td>2</td>
+                        <td>Ksh 300</td>
+                        <td>ksh 400</td>
+                    </tr>
+                    <tr>
+                        <td>pen</td>
+                        <td>4</td>
+                        <td>Ksh 20</td>
+                        <td>Ksh 700</td>
+                    </tr>
+                    <tr>
+                        <th colspan="3">Total</th>
+                        <th>Ksh 700</th>
+                    </tr>
+                </table>
+                </div>
+                    
+            
+
+            <div class="side_bar">
+                <p>During the Paleolithic Period, which lasts from the beginnings of human life until about 10,000 BCE, people were nomads
+                    .<br> lived in groups of 20 -30, and spent most of their time hunting and gathering.  In these groups, work was divided<br>
+                     between men and women, with the men hunting game animals, and women gathering fruits, berries,<br> and other edibles.
+                    These early peoples developed simple tools such as, spears and axes made from bone,<br> wood, and stone.  Human beings lived in
+                     this manner from earliest times until about 10,000 BCE, when they started to cultivate crops<br> and domesticate animals.
+                    This is known as the Neolithic Revolution. </p>
+                    <img width="40%" src="https://cloudfront-us-east-2.images.arcpublishing.com/reuters/SJ2IFECEXBIRJDSNLP2HPZJHIQ.jpg" alt="">
+        
+            </div>
+
+            <div class="footer">
+                Copyright BBIT 2.1 &copy; Allrights reserved
+            
+            </div>
+
+    </body>
+
+</html>
