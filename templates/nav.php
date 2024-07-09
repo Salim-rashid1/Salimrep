e7f4e47e130716152a8998b8ad4957395a52db50
Salim-rashid1 <salim.muchemi@strathmore.edu>
2024-07-09 20:21:39 +0300

    nav

--- templates/nav.php
+++ templates/nav.php
@@ -1,6 +1,6 @@
 <div class="toplink">
             <a href="./">Home</a> | <a href="project3.php">Menu</a> | <a href="contacts.php">Contact Us</a> | <a href="Form.php">About us</a> |  
-             | <a href="./">Directions</a> | <a href="view_messages.php">View Messages</a>
+             | <a href="directions.php">Directions</a> | <a href="view_messages.php">View Messages</a>
             
             
             <div class="toplink-right">
